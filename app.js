579eae90b66e9714153626246b7a8bbd374a0cd2
Puyo Julia <puyo.julia418@gmail.com>
2021-07-29 14:14:25 -0300

    Primeras entrega Js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su Apellido");
-console.log("Welcome" + " " + nombre + " " + apellido + " " + "to this page");
+console.log("Welcome " + nombre + " " + apellido + " to this page");
 let fecha = prompt("Ingrese el año actual");
 let nacimiento = prompt("Ingrese su año de Nacimiento");
-let edad = fecha - nacimiento;
-console.log("Tenes" + " " + edad + " " + "años");
-
-if (edad >= 18) {
+let edad = fecha - nacimieto;
+let verificacion = confirm("¿Seguro queres continuar?");
+console.log("Tenes " + edad + " años");
+if (edad >= 20 && verificacion) {
   console.log("Puedes continuar en la pagina");
 } else {
-  console.log("Usted es menor de edad, no puede continuar por acá");
+  console.log("Usted es menor de edad o no quiere continuar por acá");
 }
-alert("Bye" + " " + nombre + " " + apellido + " " + "volve pronto");
+alert("Bye " + nombre + " " + apellido + " volve pronto");
