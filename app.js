40a0a5721bfb3503ccca4f8f17d150c3a44394ea
Puyo Julia <puyo.julia418@gmail.com>
2021-07-28 23:36:34 -0300

    primera entrega JS

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ let fecha = prompt("Ingrese el año actual");
 let nacimiento = prompt("Ingrese su año de Nacimiento");
 let edad = fecha - nacimiento;
 console.log("Tenes" + " " + edad + " " + "años");
+
 if (edad >= 18) {
-  console.log("Puedes realizar la compra");
+  console.log("Puedes continuar en la pagina");
 } else {
-  console.log("Usted es menor de edad, no puede comprar por acá");
+  console.log("Usted es menor de edad, no puede continuar por acá");
 }
 alert("Bye" + " " + nombre + " " + apellido + " " + "volve pronto");
