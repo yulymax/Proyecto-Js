feb5e22524544a80a105d776563440ab38259c10
Puyo Julia <puyo.julia418@gmail.com>
2021-08-07 10:21:23 -0300

    Mi home banking

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-let nombre = prompt("Ingrese su Nombre");
-let apellido = prompt("Ingrese su Apellido");
-console.log("Welcome " + nombre + " " + apellido + " to this page");
-let fecha = prompt("Ingrese el año actual");
-let nacimiento = prompt("Ingrese su año de Nacimiento");
-let edad = fecha - nacimiento;
-let verificacion = confirm("¿Seguro queres continuar?");
-console.log("Tenes " + edad + " años");
-if (edad >= 20 && verificacion) {
-  console.log("Puedes continuar en la pagina");
-} else {
-  console.log("Usted es menor de edad o no quiere continuar por acá");
+let passwordDefault = "july005";
+let password;
+let saldoDisponible = 2500;
+let monto;
+let operacion;
+
+let nombre = prompt("Ingrese su nombre");
+
+password = prompt("Ingrese contraseña");
+
+function mostrarSaldo() {
+  alert(`Su saldo es $${saldoDisponible}`);
 }
-alert("Bye " + nombre + " " + apellido + " volve pronto");
+function transferencia(montoAtransferir) {
+  saldoDisponible = saldoDisponible - montoAtransferir;
+}
+
+function deposito(montoAdepositar) {
+  saldoDisponible = saldoDisponible + montoAdepositar;
+}
+
+if (password == passwordDefault) {
+  alert(`Bienvenid/o ${nombre} su saldo es $${saldoDisponible}`);
+  operacion = Number(
+    prompt(
+      "Si usted desea realizar una operacion, siga los siguiente pasos: 1 Si usted desea realizar una transferencia. 2 Si desea depositar "
+    )
+  );
+
+  switch (operacion) {
+    case 1:
+      alert("Usted desea transferir ");
+      monto = Number(prompt("¿Que monto desea transferir ?"));
+      transferencia(monto);
+      mostrarSaldo();
+
+      break;
+    case 2:
+      alert("Usted desea depositar");
+      monto = Number(prompt("¿Que monto desea depositar?"));
+      deposito(monto);
+      mostrarSaldo();
+      break;
+    default:
+      alert("Usted no desea realizar una operacion hoy");
+  }
+} else {
+  alert(`Contraseña incorrecta, comuniquese para reestablecer contraseña`);
+}
