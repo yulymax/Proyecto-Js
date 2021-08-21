e8db2d8f18398be9cdf8ae66a94d55bb12653b27
Puyo Julia <puyo.julia418@gmail.com>
2021-08-20 23:05:59 -0300

    DOM

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 let passwordDefault = "july005";
 let saldoDisponible = 2500;
-
-function mostrarSaldo() {
-  alert(`Su saldo es $${saldoDisponible}`);
+//Aplique Dom aca
+function mostrarSaldo(saldo) {
+  alert(`Su saldo es $${saldo}`);
+  let saldoCuentaDom = document.getElementById("saldo-cuenta");
+  saldoCuentaDom.innerHTML = saldo;
 }
 
 function transferencia(montoAtransferir) {
@@ -13,30 +15,27 @@ function deposito(montoAdepositar) {
   saldoDisponible = saldoDisponible + montoAdepositar;
 }
 
-function mostrarOperaciones() {
-  let operacion = Number(
-    prompt(
-      "Si usted desea realizar una operacion, siga los siguiente pasos: 1 Si usted desea realizar una transferencia. 2 Si desea depositar "
-    )
-  );
+function mostrarOperaciones(operacion) {
   let monto;
   switch (operacion) {
     case 1:
       alert("Usted desea transferir ");
       monto = Number(prompt("¿Que monto desea transferir ?"));
       transferencia(monto);
-      mostrarSaldo();
+      mostrarSaldo(saldoDisponible);
 
       break;
     case 2:
       alert("Usted desea depositar");
       monto = Number(prompt("¿Que monto desea depositar?"));
       deposito(monto);
-      mostrarSaldo();
+      mostrarSaldo(saldoDisponible);
       break;
     default:
       alert("Usted no desea realizar una operacion hoy");
   }
 }
 
-loginUsuario();
+let dni = prompt("Ingrese su dni");
+let clave = prompt("Ingrese su clave");
+loginUsuario(dni, clave);
